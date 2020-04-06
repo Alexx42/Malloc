186471aa670780a8621b697e234ec88d6d66d770
Alexx42 <legoff.alexandre06@gmail.com>
2020-04-06 03:31:47 -0400

    modify the place of the deletion to not overwrite memory

--- sources/memory_chunk.c
+++ sources/memory_chunk.c
@@ -1,14 +1,17 @@
 #include "../includes/malloc.h"
 
 void			*write_meta_data(void *ptr, size_t size) {
-	f_list *tmp;
-	
+	f_list		*tmp;
+	m_block		*block;
+
+	block = find_corresponding_block(ptr);
+	block->h_block.used += S_ALLOC(f_list) + size;
+	delete_element_f_list(ptr);
 	tmp = (f_list *)ptr;
 	tmp->f.data.d.size = size;
 	tmp->f.data.d.magic = MAGIC;
-	delete_element_f_list(ptr);
-	// add_element_f_list(size);
-	return (void *)ptr + S_ALLOC(m_data);
+	add_element_f_list(ptr, size, block->h_block.size - block->h_block.used);
+	return (void *)((void *)tmp + S_ALLOC(f_list));
 }
 
 void			*find_available_memory_chunk(size_t size) {

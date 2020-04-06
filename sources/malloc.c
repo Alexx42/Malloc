1a20013545de46979a09448d9a485458848bd535
Alexx42 <legoff.alexandre06@gmail.com>
2020-04-06 04:13:19 -0400

    added tests for malloc

--- sources/malloc.c
+++ sources/malloc.c
@@ -21,16 +21,15 @@ void			*my_malloc(size_t size) {
 
 int				main(void) {
 	char *ptr = my_malloc(10);
-	char *ptr2 = my_malloc(1000000);
+	char *ptr2 = my_malloc(5000);
 	char *ptr3 = my_malloc(15000);
 	ptr[0] = 'a';
 	ptr[1] = 'b';
 	ptr[2] = '\0';
-	ptr2[0] = 'a';
-	ptr2[1] = 'b';
-	ptr2[2] = '\0';
-	printf("%s\n", ptr);
-	printf("%s\n", ptr2);
-	(void)ptr3;
+	ptr2[0] = 'P';
+	ptr3[0] = 'a';
+	ptr3[1] = 'b';
+	ptr3[2] = '\0';
+	printf("ptr1: %s\nptr2: %s\nptr3: %s\n", ptr, ptr2, ptr3);
 	return  0;
 }

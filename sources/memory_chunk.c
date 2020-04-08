be774f81da45ca11136cd7f4559d0bb2ab6a8d59
Alexx42 <legoff.alexandre06@gmail.com>
2020-04-08 01:52:29 -0400

    solved bug with the add element f list

--- sources/memory_chunk.c
+++ sources/memory_chunk.c
@@ -18,21 +18,32 @@ void			*write_meta_data(void *ptr, size_t size) {
 	tmp = (f_list *)ptr;
 	tmp->f.data.d.size = size;
 	tmp->f.data.d.magic = MAGIC;
-	if (block->h_block.size == (size_t)SMALL_ZONE || block->h_block.size == (size_t)TINY_ZONE) {}
-		add_element_f_list(ptr, size, block->h_block.size - block->h_block.used);
+	if (block->h_block.size == (size_t)SMALL_ZONE || block->h_block.size == (size_t)TINY_ZONE)
+		add_element_f_list(ptr + S_ALLOC(f_list) + tmp->f.data.d.size, block->h_block.size - block->h_block.used);
 	return (void *)((void *)tmp + S_ALLOC(f_list));
 }
 
 void			*find_available_memory_chunk(size_t size) {
 	f_list		*tmp;
 	m_block		*b;
+	f_list		*min;
 
 	tmp = g_flist;
+	min = tmp;
 	while (tmp) {
 		b = find_corresponding_block((void *)tmp);
-		if (valid_current_block(b, size) == EXIT_SUCCESS && tmp->f.data.d.size > size)
-			return write_meta_data((void *)(tmp), size);
+		if (valid_current_block(b, size) == EXIT_SUCCESS && tmp->f.data.d.size > size) {
+			if (min == NULL || tmp->f.data.d.size < min->f.data.d.size)
+				min = tmp;
+		}
 		tmp = tmp->f.next;
+	}
+	
+	if (min != NULL) {
+		if (min->f.data.d.size > size){
+			add_element_f_list((void *)min + S_ALLOC(f_list) + min->f.data.d.size, min->f.data.d.size - size);
+		}
+		return write_meta_data((void *)min, size);
 	}
 	return NULL;
 }
